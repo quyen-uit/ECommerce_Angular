d50175d08045a986888cf03f3e50912ca5309798
Quyen Dang <45599201+quyen-uit@users.noreply.github.com>
2025-06-23 00:30:30 +0700

    update to angular 16

--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -1,9 +1,19 @@
-export interface Product {
-  id: number,
+import { ProductSize } from "../enums/productSize";
+
+export interface CreateProduct {
   name: string;
   description: string;
   price: number;
   photoUrl: string;
+  categoryId: number;
+  productBrandId: number;
+  isNew: boolean;
+  isTrending: boolean;
+  productSize: ProductSize;
   productType: string;
   productBrand: string;
+}
+
+export interface Product extends CreateProduct {
+  id: number
 }
